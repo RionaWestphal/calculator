1a3bc9a58a3e74a8e3dc1bab347693756218bab5
Riona Westphal <riona.westphal@gmail.com>
2022-08-03 22:19:27 +0900

    Add operate function
    The operate(operator, num1, num2) function calls one of the elementary
    functions (add/subract/multiply/divide) based on its parameters.

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ function multiply(x,y) {
 function divide(x,y) {
     return x / y;
 }
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case("+"): return num1 + num2; break;
+        case("-"): return num1 - num2; break;
+        case("*"): return num1 * num2; break;
+        case("/"): return num1 / num2; break;
+    }
+}
